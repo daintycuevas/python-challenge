1b52ac20ae7c908a4e2881923c37cd33fed241cb
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2020-11-14 08:13:09 -0500

    Message(commit on 'main')

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,11 @@
 import os
 import csv
+
+budget_path = os.path.join(r'.\Resources\budget_data.csv')
+budget_csv = (budget_path)
+
+with open(budget_csv, 'r') as text:
+    lines = text.read()
+    print(lines)
+
+    
