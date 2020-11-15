81fd2e3ed640706a2d246854e000b5655213dbbf
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2020-11-14 21:52:28 -0500

    Message(jupyter commit)

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,12 +2,11 @@ import os
 
 import csv
 
-budget_path = os.path.join(r'.\Resources\budget_data.csv')
+budget_path = os.path.join('..\Desktop\budget_data.csv', 'r')
 
-budget_csv = (budget_path)
+budget_csv = budget_path
 
+lines = 0
 with open(budget_csv, 'r') as text:
     lines = text.read()
     print(lines)
-
-row_count = len(budget_csv)
